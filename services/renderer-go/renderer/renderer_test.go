87e2f067c8804be74562c47e45b12dbe8721cc48
intern <intern@hatenanoMacBook-Pro.local>
2021-08-18 17:30:54 +0900

    [ADD]リスト記法のテスト追加

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -8,12 +8,14 @@ import (
 )
 
 func Test_Header(t *testing.T) {
-	src := `# Test`
+	src := "# Test"
 	html, err := Render(context.Background(), src)
 	assert.NoError(t, err)
-	assert.Equal(t, `<h1>Test</h1>`, html)
+	assert.Equal(t, "<h1>Test</h1>\n", html)
 }
-func Test_(t *testing.T) {
-	src := ``
-	
+func Test_List(t *testing.T) {
+	src := "- list1"
+	html, err := Render(context.Background(), src)
+	assert.NoError(t, err)
+	assert.Equal(t ,"<ul>\n<li>list1</li>\n</ul>\n", html)
 }
