a431d1588d3a9ca524aa5e29db29cb539e5d9f32
intern <intern@hatenanoMacBook-Pro.local>
2021-08-18 17:49:17 +0900

    [ADD]リンク記法のテスト追加

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -19,3 +19,9 @@ func Test_List(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t ,"<ul>\n<li>list1</li>\n</ul>\n", html)
 }
+func Test_Link(t *testing.T) {
+	src := "[はてなブックマーク](https://b.hatena.ne.jp/)"
+	html, err := Render(context.Background(), src)
+	assert.NoError(t, err)
+	assert.Equal(t ,"<p><a href=\"https://b.hatena.ne.jp/\">はてなブックマーク</a></p>\n", html)
+}
