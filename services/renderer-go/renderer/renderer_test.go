84f23fb99d624d40af78612bbdaccb8b861f06ee
intern <intern@hatenanoMacBook-Pro.local>
2021-08-18 17:15:36 +0900

    srcの部分が間違えていたので修正

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -7,9 +7,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test(t *testing.T) {
-	src := `# Test\n`
+func Test_Header(t *testing.T) {
+	src := `# Test`
 	html, err := Render(context.Background(), src)
 	assert.NoError(t, err)
 	assert.Equal(t, `<h1>Test</h1>`, html)
+}
+func Test_(t *testing.T) {
+	src := ``
+	
 }
