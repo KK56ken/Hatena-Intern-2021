471a9bfed26c67ed3435a97e1a93fe1da477043a
intern <intern@hatenanoMacBook-Pro.local>
2021-08-18 18:15:11 +0900

    [FIX]goの命名規則にそって変更

--- services/renderer-go/renderer/renderer_test.go
+++ services/renderer-go/renderer/renderer_test.go
@@ -7,19 +7,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_Header(t *testing.T) {
+func TestHeader(t *testing.T) {
 	src := "# Test"
 	html, err := Render(context.Background(), src)
 	assert.NoError(t, err)
 	assert.Equal(t, "<h1>Test</h1>\n", html)
 }
-func Test_List(t *testing.T) {
+func TestList(t *testing.T) {
 	src := "- list1"
 	html, err := Render(context.Background(), src)
 	assert.NoError(t, err)
 	assert.Equal(t ,"<ul>\n<li>list1</li>\n</ul>\n", html)
 }
-func Test_Link(t *testing.T) {
+func TestLink(t *testing.T) {
 	src := "[はてなブックマーク](https://b.hatena.ne.jp/)"
 	html, err := Render(context.Background(), src)
 	assert.NoError(t, err)
