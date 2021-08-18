f4b0a4cffbb5edf6c99de0dcb8028146d7c283cf
intern <intern@hatenanoMacBook-Pro.local>
2021-08-18 18:17:13 +0900

    [FIX]コメントの削除

--- services/renderer-go/renderer/renderer.go
+++ services/renderer-go/renderer/renderer.go
@@ -3,14 +3,8 @@ package renderer
 import (
 	"bytes"
 	"context"
-	// "html/template"
-	// "regexp"
 	"github.com/yuin/goldmark"
-	// "github.com/yuin/goldmark/parser"
 )
-// var urlRE = regexp.MustCompile(`https?://[^\s]+`)
-// var linkTmpl = template.Must(template.New("link").Parse(`<a href="{{.}}">{{.}}</a>`))
-
 
 // Render は受け取った文書を HTML に変換する
 func Render(ctx context.Context, src string) (string, error) {
@@ -23,14 +17,4 @@ func Render(ctx context.Context, src string) (string, error) {
 	}
 
 	return w.String(), nil
-	
-
-	// html := urlRE.ReplaceAllStringFunc(src, func(url string) string {
-	// 	err := linkTmpl.ExecuteTemplate(&w, "link", url)
-	// 	if err != nil {
-	// 		return url
-	// 	}
-	// 	return w.String()
-	// })
-	// return html, nil
 }
